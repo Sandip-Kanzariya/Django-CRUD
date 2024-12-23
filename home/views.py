389ed5482c10645d0e04fb976f1d2427573abfe6
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-12-23 19:28:11 +0530

    base template, urls and views for product list, product list displayed

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from home.models import Product
 
 # Create your views here.
+
+def products(request):
+    # Retrieve all products from the database
+    product_list = Product.objects.all()
+
+    # Pass the products to the template
+    return render(request, 'products.html', {'products': product_list})
