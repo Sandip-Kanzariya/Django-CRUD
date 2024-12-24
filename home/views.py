84121199d5d41be335e0ed76cc94a693496992f7
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-12-24 12:12:08 +0530

    update product functional

--- home/views.py
+++ home/views.py
@@ -39,6 +39,7 @@ def create_product(request):
     return render(request, 'create-product.html', {'product_type' : PRODUCT_TYPE, 'status':STATUS})
 
 def update_product(request, pk):
+
     # Find the product with id = pk
     try:
         product = Product.objects.get(pk=pk)
@@ -46,7 +47,32 @@ def update_product(request, pk):
         # Redirect to 404.html explicitly
         return render(request, '404.html', status=404)
 
-    return render(request, 'update-product.html', {'product':product})
+    # if Submission happens then
+    if request.method == "POST":
+        name = request.POST.get('name')
+        category = request.POST.get('category')
+        price = request.POST.get('price')
+        status = request.POST.get('status')
+
+        # category = category.title()
+        # Select Category based on value
+        for pt in PRODUCT_TYPE:
+            if pt[0] == category:
+                category = pt[1]
+                break
+
+        # Update the product with the new data
+        product.name = name
+        product.category = category
+        product.price = price
+        product.status = status
+
+        # Save the updated product to the database
+        product.save()
+
+        return redirect('products')
+
+    return render(request, 'update-product.html', {'product':product, 'product_type' : PRODUCT_TYPE, 'status':STATUS})
 
 
 def delete_product(request, pk):
