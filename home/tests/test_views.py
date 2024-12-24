ccaa840ac340dedb0370e8aa330ff06e7b65e86b
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-12-24 16:39:12 +0530

    test suit for update product, tearDown added

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -4,13 +4,24 @@ from ..models import Product, PRODUCT_TYPE, STATUS
 
 class TestProductViews(TestCase):
 
+    # Automatic invocation
     def setUp(self):
         """Set up reusable test data."""
         self.client = Client()
-
+        self.product = Product.objects.create(
+            name="Test Product",
+            category="Electronics",
+            price=100.00,
+            status=1
+        )
         self.products_url = reverse('products')
         self.create_product_url = reverse('create_product')
+        self.update_url = reverse('update_product', args=[self.product.id]) # Check for added product in setUp
 
+    # So, it not conflict with state of previous test case
+    def tearDown(self):
+        """Clean up after each test."""
+        Product.objects.all().delete()
 
     def test_products_GET(self):
         """Test the GET request to the 'products' view."""
@@ -26,10 +37,12 @@ class TestProductViews(TestCase):
             'price': '10.50',
             'status': STATUS[1][0],  # Using 'Available' as status
         })
-
         # Assert that the product was created
-        self.assertEquals(Product.objects.count(), 1)
-        product = Product.objects.first()
+        self.assertEquals(Product.objects.count(), 2)  # 1 product from setUp() + new one
+
+        # Fetch the newly created product
+        product = Product.objects.exclude(id=self.product.id).first()  # Exclude the initial product created in setUp
+
         self.assertEquals(product.name, 'Banana Chips')
         self.assertEquals(product.category, PRODUCT_TYPE[3][1])  # 'Food'
         self.assertEquals(product.price, 10.50)
@@ -38,3 +51,39 @@ class TestProductViews(TestCase):
         # Assert redirection to the products page
         self.assertRedirects(response, self.products_url)
 
+    def test_update_product_GET(self):
+        """Test the GET request to 'update_product' to update product"""
+        response = self.client.get(self.update_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'update-product.html')
+
+    def test_update_product_POST_valid_data(self):
+        """Test the POST request to 'update_product' to update product"""
+        response = self.client.post(self.update_url, {
+            'name': 'Updated Product',
+            'category': 'Fashion',
+            'price': 150.00,
+            'status': 0
+        })
+
+        # Refresh the product from the database to get the updated values
+        self.product.refresh_from_db()
+
+        self.assertEquals(response.status_code, 302)  # Redirect to products
+        self.assertEquals(self.product.name, 'Updated Product')
+        self.assertEquals(self.product.category, 'Fashion')  # Human-readable category
+        self.assertEquals(self.product.price, 150.00)
+        self.assertEquals(self.product.status, 0)
+
+    def test_update_product_POST_invalid_product(self):
+        """Test the POST request to 'update_product' to update product which is not exist."""
+        invalid_update_url = reverse('update_product', args=[999])
+        response = self.client.post(invalid_update_url, {
+            'name': 'Invalid Product',
+            'category': 'Fashion',
+            'price': 150.00,
+            'status': 0
+        })
+
+        self.assertEquals(response.status_code, 404) # Page not found
+        self.assertTemplateUsed(response, '404.html')
