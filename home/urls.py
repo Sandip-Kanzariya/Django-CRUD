ef3593af89862c4a7a7d0e5651a515c5e1c4e09b
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-12-24 09:07:52 +0530

    delete and update product urls, views, delete functional, 404

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.products, name='products'),
+    path('update/<int:pk>', views.update_product, name="update_product"),
+    path('delete/<int:pk>', views.delete_product, name="delete_product"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from home.models import Product
 
 # Create your views here.
@@ -9,3 +9,29 @@ def products(request):
 
     # Pass the products to the template
     return render(request, 'products.html', {'products': product_list})
+
+def update_product(request, pk):
+    # Find the product with id = pk
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        # Redirect to 404.html explicitly
+        return render(request, '404.html', status=404)
+
+    return render(request, 'update-product.html', {'product':product})
+
+
+def delete_product(request, pk):
+
+    # Find the product with id = pk
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        # Redirect to 404.html explicitly
+        return render(request, '404.html', status=404)
+
+    if request.method == "POST":
+        product.delete()
+        return redirect('products')
+    else:
+        return redirect('products')
